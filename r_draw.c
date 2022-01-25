d16d3a28b3a8eab1579f263b6e79290b4a7b2fb4
nukeykt <alexeytf2@gmail.com>
2022-01-26 07:10:12 +0900

    gpl headers

--- r_draw.c
+++ r_draw.c
@@ -1,3 +1,20 @@
+//
+// Copyright (C) 1993-1996 Id Software, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+
 // R_draw.c
 
 extern int _wp1, _wp2, _wp3, _wp4, _wp5, _wp6, _wp7, _wp8, _wp9, _wp10;
